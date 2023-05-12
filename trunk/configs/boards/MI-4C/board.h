23b1db8a53d0019ca2c750d109a1af377329af2c
tengpeng1888 <133008074+tengpeng1888@users.noreply.github.com>
2023-05-12 21:22:05 +0800

    Update board.h

--- trunk/configs/boards/MI-4C/board.h
+++ trunk/configs/boards/MI-4C/board.h
@@ -12,9 +12,9 @@
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL 
 #define BOARD_GPIO_LED_WIFI	11
-#define BOARD_GPIO_LED_POWER	24	/* 24: blue, 26: yellow, 29: red */
+#define BOARD_GPIO_LED_POWER	24
 #undef  BOARD_GPIO_LED_LAN
-#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_WAN   24
 #define BOARD_HAS_5G_11AC	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
