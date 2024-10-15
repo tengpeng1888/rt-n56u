76a76e4593a7e6af7d5cfaeee8cba10156de454e
tengpeng1888 <133008074+tengpeng1888@users.noreply.github.com>
2024-10-15 11:13:23 +0800

    Update board.h

--- trunk/configs/boards/ZXPJ-Z1/board.h
+++ trunk/configs/boards/ZXPJ-Z1/board.h
@@ -6,20 +6,20 @@
 #define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
 #define BOARD_VENDOR_URL	"http://www.mi.com/"
 #define BOARD_MODEL_URL		"http://www.mi.com/miwifilite"
-#define BOARD_BOOT_TIME		25
+#define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	38
 #undef BOARD_GPIO_LED_ALL 
 #undef BOARD_GPIO_LED_SW2G
 #undef BOARD_GPIO_LED_SW5G
-#define  BOARD_GPIO_LED_POWER	39
+#define  BOARD_GPIO_LED_POWER	40
 #undef BOARD_GPIO_LED_WAN
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
-#define BOARD_NUM_ETH_LEDS	1
-#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
 #define BOARD_HAS_EPHY_L1000	0
 #define BOARD_HAS_EPHY_W1000	0
