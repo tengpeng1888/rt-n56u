c4b1c04be7872d33baede01a9567dd7d64c0cbf7
tengpeng1888 <133008074+tengpeng1888@users.noreply.github.com>
2024-11-16 19:28:40 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -773,6 +773,7 @@ struct nvram_pair router_defaults[] = {
 #endif
 
 #if defined(CONFIG_RALINK_MT7621) || (defined(CONFIG_RALINK_MT7620) || && !defined(BOARD_N14U))
+#if defined(USE_MT7615_AP) || (USE_MT7915_AP) // hwnat is disabled by default
 	{ "hw_nat_mode", "2" },
 #else
 	{ "hw_nat_mode", "4" },
