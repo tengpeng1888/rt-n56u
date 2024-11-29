478331b29cf538f473f626faef3d551d62574094
tengpeng1888 <133008074+tengpeng1888@users.noreply.github.com>
2024-11-29 23:30:49 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -269,7 +269,7 @@ struct nvram_pair router_defaults[] = {
 #if defined(BOARD_HAS_2G_11AX) && BOARD_HAS_2G_11AX
 	{ "rt_gmode", "6" },			/* b/g/n/ax mixed */
 #else
-	{ "rt_gmode", "5" },			/* g/n mixed */
+	{ "rt_gmode", "2" },			/* g/n mixed */
 #endif
 	{ "rt_mcs_mode", "0" },
 	{ "rt_channel", "13" },
