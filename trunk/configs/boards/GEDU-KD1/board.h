4ac6a10473af3e7aac7a85b26665f262617495c7
tengpeng1888 <133008074+tengpeng1888@users.noreply.github.com>
2025-09-21 21:59:10 +0800

    Add files via upload

---
+++ trunk/configs/boards/GEDU-KD1/board.h
@@ -0,0 +1,32 @@
+/* GEDU-KD1 */
+
+#define BOARD_PID		"GEDU-KD1"
+#define BOARD_NAME		"GEDU-KD1"
+#define BOARD_DESC		"GEDU-KD1 Wireless Router"
+#define BOARD_VENDOR_NAME	"HiWiFi/Gee Technology"
+#define BOARD_VENDOR_URL		"http://www.hiwifi.com/"
+#define BOARD_MODEL_URL		"http://www.hiwifi.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME		120
+#undef  BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET    18
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	16
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC       1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY      4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	1
